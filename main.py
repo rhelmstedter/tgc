00a80d479430533ecfb90993e647c921fa96611e
rhelmstedter <rhelmstedter@gmail.com>
2021-05-09 17:12:53 -0700

    send email to student with their graphic

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from background import *
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from tkinter import *
+import keyring
+import smtplib, ssl
 import subprocess
 import turtle
-from tkinter import *
 
 
-FILE_NAME = "ALTurtle"
+FILE_NAME = "AMTurtle"
 
 screen = turtle.Screen()
-screen.bgcolor('black')
+screen.bgcolor("black")
 turtle.speed(0)
 draw_background()
 
@@ -17,5 +22,49 @@ turtle.ht()
 
 screen.getcanvas().postscript(file=f"{FILE_NAME}.eps")
 
-subprocess.run(["inkscape", f"--export-filename={FILE_NAME}.svg", f"{FILE_NAME}.eps"])
+subprocess.run(["inkscape", f"--export-filename=images/{FILE_NAME}.svg", f"{FILE_NAME}.eps"])
 subprocess.run(["rm", f"{FILE_NAME}.eps"])
+subprocess.run(["inkscape", f"images/{FILE_NAME}.svg"])
+
+# Setting up credentials
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+gmail = 'russell.helmstedter@venturaedu.org'
+PASSWORD = keyring.get_password("system", "venturaedu")
+
+# Setting up message
+message = MIMEMultipart('mixed')
+message['From'] = 'Contact <{sender}>'.format(sender = gmail)
+message['To'] = 'amaroly@venturaedu.org'
+message['Subject'] = 'High Resolution Turtle Graphic'
+
+# Write the email
+msg_content = '<p>Hello,<br>Here is your spiral as a high resolutuon svg file. I hope you enjoy.<br><br>Mr. Helmstedter</p>\n'
+body = MIMEText(msg_content, 'html')
+message.attach(body)
+
+# Find the attachment
+attachmentPath = f"images/{FILE_NAME}.svg"
+try:
+	with open(attachmentPath, "rb") as attachment:
+		p = MIMEApplication(attachment.read(),_subtype="svg")	
+		p.add_header('Content-Disposition', "attachment; filename= %s" % attachmentPath.split("/")[-1]) 
+		message.attach(p)
+except Exception as e:
+	print(str(e))
+
+# Send email
+msg_full = message.as_string()
+context = ssl.create_default_context()
+
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+	server.ehlo()  
+	server.starttls(context=context)
+	server.ehlo()
+	server.login(gmail, PASSWORD)
+	server.sendmail(gmail, message['To'], msg_full)
+	server.quit()
+
+print("email sent out successfully")
+
+
--- studentspiral.py
+++ studentspiral.py
@@ -1,2 +1,15 @@
 import turtle
 
+
+sides = 10
+distance = 0.1
+
+colors = ["white", "red", "white"]
+count = 0.01
+
+for _ in range(185*sides):
+    count += 0.0015
+    turtle.color(colors[int(count)])
+    distance += 0.1
+    turtle.forward(distance)
+    turtle.right(360 / sides + 1)
